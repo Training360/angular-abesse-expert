bb61d53021fec21fe648166aee642d3e915b8891
unknown <j.cserko@gmail.com>
2020-09-23 14:00:27 +0200

    Feat: test table.

--- building/src/app/app.component.ts
+++ building/src/app/app.component.ts
@@ -1,44 +1,20 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import flatten from 'lodash-es/flatten';
-import difference from 'lodash-es/difference';
-import cloneDeep from 'lodash-es/cloneDeep';
-import { Lodash } from './decorator/lodash.decorator';
+import { Observable } from 'rxjs';
+import { User } from './model/user';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-@Lodash(['flatten', 'difference', 'cloneDeep'])
 export class AppComponent implements OnInit {
-  title = 'building';
-  flatten: typeof flatten;
-  difference: typeof difference;
-  cloneDeep: typeof cloneDeep;
+  title = 'perf';
+  users$: Observable<User[]> = this.http.get<User[]>('http://localhost:3000/users');
 
-  constructor() {
-  }
+  constructor(
+    private http: HttpClient,
+  ) {}
 
-  ngOnInit(): void {
-    const test = [1, 2, [4, 5]];
-    console.log( this.flatten(test) );
-/*
-    const a1 = [1, 4, 7];
-    const a2 = [1, 5, 7];
-    console.log( difference(a1, a2) );
-
-    const user = {
-      name: 'Pistike',
-      skills: [
-        'eat',
-        'drink',
-        'play'
-      ]
-    };
-
-    const user2 = cloneDeep(user);
-    console.log(user, user2); */
-
-  }
-
+  ngOnInit(): void {}
 }
--- building/src/app/app.module.ts
+++ building/src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { ChildComponent } from './page/child/child.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,9 @@ import { ChildComponent } from './page/child/child.component';
     ChildComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    NgbModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
